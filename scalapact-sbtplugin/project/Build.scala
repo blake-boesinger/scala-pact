de95b133d53c1ecbd70e183a2085335e19eec79c
Blake Boesinger <blake.boesinger@itv.com>
2016-03-09 15:08:30 +0000

    resovlers and publishTo

--- scalapact-sbtplugin/project/Build.scala
+++ scalapact-sbtplugin/project/Build.scala
@@ -4,14 +4,15 @@ import Keys._
 object Build extends sbt.Build with BuildExtra {
   lazy val sbtIdea = Project("scalapact-plugin", file("."), settings = mainSettings)
 
-  resolvers += "ITV Repo" at " http://itvrepos.artifactoryonline.com/itvrepos/cps-libs"
 
-  publishTo := Some("Artifactory Realm" at "https://itvrepos.artifactoryonline.com/itvrepos/cps-libs")
+
 
   lazy val mainSettings: Seq[Def.Setting[_]] = Seq(
     sbtPlugin := true,
     organization := "com.itv.plugins",
     name := "scalapact-plugin",
+    resolvers += "ITV Repo" at " http://itvrepos.artifactoryonline.com/itvrepos/cps-libs",
+    publishTo := Some("Artifactory Realm" at "https://itvrepos.artifactoryonline.com/itvrepos/cps-libs"),
     version := "0.1.2-SNAPSHOT",
     sbtVersion in Global := "0.13.11",
     scalaVersion in Global := "2.10.6",
@@ -22,7 +23,7 @@ object Build extends sbt.Build with BuildExtra {
       "org.http4s" %% "http4s-blaze-server" % "0.11.3" withSources() withJavadoc(),
       "org.http4s" %% "http4s-dsl"          % "0.11.3" withSources() withJavadoc(),
       "org.http4s" %% "http4s-argonaut"     % "0.11.3" withSources() withJavadoc(),
-      "com.itv" % "scalapact-core_2.10" % "0.1.2-SNAPSHOT",
+      "com.itv" % "scalapact-core_2.10" % "0.1.5",
       "org.scalatest" %% "scalatest" % "2.2.6" % "test",
       "org.scalaj" %% "scalaj-http" % "2.2.1"
     )
